43aa43ed44c6138104086202bc0046c89c919ff4
Annie <annietang.california@gmail.com>
2025-07-13 15:49:38 -0700

    Initial commit

---
+++ src/main/java/com/synex/contoller/HelloContoller.java
@@ -0,0 +1,12 @@
+package com.synex.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContoller {
+	@GetMapping("/hello")
+    public String sayHi() {
+        return "Hi from Annie!";
+    }
+}
